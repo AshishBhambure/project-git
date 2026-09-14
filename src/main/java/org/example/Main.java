8fc6f060e6e3f5cf4ecab3dbab4af289b88292e6
AshishBhambureAsDevA <ashishbhambure1@gmail.com>
2026-09-14 21:54:39 +0530

    DevA Commmits for changes requested by DevB

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,12 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
 
         System.out.println("DeveloperA Says Hello Again! Doing changes As was suggested");
+        System.out.println("DevB Says: Hello DevA");
+        System.out.println("Some Changes");
+
+
+
+
+
     }
 }
